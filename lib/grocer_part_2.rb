e8b32b186d84780355fecd84950be3599e566e4b
Jeremy Winn <jeremyjwinn@gmail.com>
2020-07-07 18:22:19 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,28 +1,22 @@
 require_relative './part_1_solution.rb'
 
-def apply_coupons(cart, coupons)
-  counter = 0 
-  while counter < coupons.length 
-  cart_item = find_item_by_name_in_collection(coupons[counter][:item], cart)
-  couponed_item_with_name = "#{coupons[counter][:item]} W/COUPON}"
-  cart_item_with_coupon = find_item_by_name_in_collection(couponed_item_with_name, cart)
-  if cart_item && cart_item[:count] >= coupons[counter][:num]
-    if cart_item_with_coupon
-      cart_item_with_coupon[:count] -= coupons[counter][:num]
-      else
-        cart_item_with_coupon = {
-          :item => couponed_item_with_name,
-          :price => coupons[counter][:cost] / coupons[counter][:num],
-          :count => coupons[counter][:num],
-          :clearence => cart_item[:clearence]
-      }
-      cart << cart_item_with_coupon
-      cart_item[:count] -= coupons[counter][:num]
-     end
-    end
-    counter += 1 
-  end
-  cart
+def apply_coupons(cart, coupons) 
+  
+  coupons.each do |coupon| 
+    coupon.each do |attribute, value| 
+      name = coupon[:item] 
+      if cart[name] && cart[name][:count] >= coupon[:num] 
+        if cart["#{name} W/COUPON"] 
+          cart["#{name} W/COUPON"][:count] += 1 
+        else 
+          cart["#{name} W/COUPON"] = {:price => coupon[:cost], 
+          :clearance => cart[name][:clearance], :count => 1} 
+        end 
+      cart[name][:count] -= coupon[:num] 
+    end 
+  end 
+end 
+  cart 
 end
 
 def apply_clearance(cart)
