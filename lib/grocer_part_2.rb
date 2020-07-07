b6fd80123f95d42d0a7b612b1cfa29154072b3c0
Jeremy Winn <jeremyjwinn@gmail.com>
2020-07-07 18:14:59 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -3,9 +3,9 @@ require_relative './part_1_solution.rb'
 def apply_coupons(cart, coupons)
   counter = 0 
   while counter < coupons.length 
-  cart_item = find_item_by_collection(coupons[counter][:item], cart)
+  cart_item = find_item_by_name_in_collection(coupons[counter][:item], cart)
   couponed_item_with_name = "#{coupons[counter][:item]} W/COUPON}"
-  cart_item_with_coupon = find_item_by_collection(couponed_item_with_name, cart)
+  cart_item_with_coupon = find_item_by_name_in_collection(couponed_item_with_name, cart)
   if cart_item && cart_item[:count] >= coupons[counter][:num]
     if cart_item_with_coupon
       cart_item_with_coupon[:count] -= coupons[counter][:num]
